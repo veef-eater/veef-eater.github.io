b795f9880e1ce294e2944d6c04096bf6f86831c9
veef-eater <big.veef.eater@gmail.com>
2020-12-19 18:16:27 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-content">
+        <div>
+          <h1>❓ What is Veef?</h1>
+          <p>The vegan ground beef substitute that is taking a very small, very specific portion of the internet by storm!</p>
+        </div>
+        <div>
+          <h1>📃 The Recipe</h1>
+          <h2>Ingredients</h2>
+          <ul>
+            <li>17g dried wild mushroom mix</li>
+            <li>2/3 cup green lentils</li>
+            <li>227g cremini mushrooms</li>
+            <li>1/4 cup walnuts</li>
+            <li>1 Tbsp flax meal</li>
+            <li>3 Tbsp nutritional yeast</li>
+            <li>1 tsp salt, plus more to taste</li>
+            <li>black pepper to taste</li>
+            <li>1/2 tsp porcini mushroom powder</li>
+            <li>1/2 vegetable bouillon cube</li>
+          </ul>
+          <h2>Instructions</h2>
+          <ol>
+            <li><strong>Rehydrate the wild mushrooms.</strong> Reserve the rehydrating liquid for cooking the lentils.</li>
+            <li><strong>Cook the lentils.</strong> Lightly salt the mushroom-water (and dilute if necessary) and bring to a boil. Cook the lentils 2 minutes shy of done, according to the package instructions. Once cooked, drain thoroughly.</li>
+            <li><strong>Toast the walnuts.</strong> While the lentils cook, toast the walnuts in a dry skillet until fragrant and darkened.</li>
+            <li><strong>Process the lot.</strong> Add the rehydrated wild mushrooms, lentils, cremini mushrooms, and toasted walnuts to a food processor and pulse until desired crumbly-ness is reached.</li>
+            <li><strong>Cook the raw Veef.</strong> Heat olive oil in a skillet over medium-high heat. Working in batches as necessary, brown the Veef mix until the mushrooms are totally cooked, about 8 minutes per side.</li>
+          </ol>
+        </div>
+        <div>
+          <h1>🍳 What can I do with the Veef?</h1>
+          <p>TODO: Put some kinda fun randomly-generated message here with a re-roll button</p>
+        </div>
+      </div>
     </div>
   );
 }
